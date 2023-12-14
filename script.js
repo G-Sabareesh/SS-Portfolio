1de935100b73d721b2eb1e1ccc85cd8ced8502fd
Sabareesh G <123008085+G-Sabareesh@users.noreply.github.com>
2023-12-14 12:31:13 +0530

    Minor bug fix

--- script.js
+++ script.js
@@ -3,13 +3,14 @@
         nav_tab.addEventListener('click', function () {
             document.querySelector('.active-tab').classList.remove('active-tab');
             this.classList.add('active-tab');
-            var navLink = nav_tab.dataset.id
+            // console.log(nav_tab.dataset.id)
+            var navLink = document.getElementById(nav_tab.dataset.id)
             if (navLink) {
-                navLink.scrollIntoViewOptions({ behavior: 'smooth' })
+                navLink.scrollIntoView({ behavior: 'smooth' })
             }
-            // if (nav_tab.id !== 'header') {
-            clickTab();
-            // }
+            if (nav_tab.dataset.id !== 'header') {
+                clickTab();
+            }
         });
     })
 })();
@@ -45,11 +46,13 @@ function clickTab() {
             document.querySelector('.nav-tabs ul').style.display = 'flex'
             nav.classList.remove('fa-bars')
             nav.classList.add('fa-xmark')
+            document.body.style.overflowY = 'hidden'
         }
         else if (nav.classList[1] == 'fa-xmark') {
             document.querySelector('.nav-tabs ul').style.display = 'none'
             nav.classList.add('fa-bars')
             nav.classList.remove('fa-xmark')
+            document.body.style.overflowY = 'scroll'
         }
     }
 }
